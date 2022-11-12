522ae03615303d82c6fcadde1801c299c292b151
flowjunkie <flowjunkie@users.noreply.github.com>
2022-11-12 11:20:01 -0800

    Initial Xcode project.

---
+++ ScanList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScanList
+//
+//  Created by Melody Prince on 11/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScanList/ScanListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanListApp.swift
+//  ScanList
+//
+//  Created by Melody Prince on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScanListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
